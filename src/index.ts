bf4f74c1c35e2d15578d97cb331e2f255d93fcf6
Odyssey346 <odyssey346@disroot.org>
2022-07-15 09:36:45 +0200

    1.0.4 then gfys
    Signed-off-by: Odyssey346 <odyssey346@disroot.org>

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { makeRequest } from './handle/request.js';  //FIXME: If tsconfig.json sp
 import chalk from  'chalk';
 
 const program = new Command();
-export const version = "1.0.3";
+export const version = "1.0.4";
 
 program
     .version(version)
